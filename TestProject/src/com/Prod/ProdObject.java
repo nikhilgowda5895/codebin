a3f041832544dde25f6faaca9800a3acf7c71cc4
NIKHILGOWDA <NIKHILGOWDA@PC-NIKHILGOWDA>
2019-03-24 05:30:08 +0530

    Production classes created and TestObject class created

---
+++ TestProject/src/com/Prod/ProdObject.java
@@ -0,0 +1,9 @@
+package com.Prod;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class ProdObject {
+	public static void main(String args[]) {
+		System.out.println("Hello Nikhil");
+	}
+}
